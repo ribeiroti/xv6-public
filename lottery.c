3952083efa8d05b8beaeb7cea416e1ce82f991c2
Bruno Ribeiro <brunoribeiroinf@gmail.com>
2018-09-18 00:17:15 -0300

    chamada de sistema alterada para testes do scheduler

--- lottery.c
+++ lottery.c
@@ -8,9 +8,33 @@ https://01siddharth.blogspot.com/2018/04/adding-system-call-in-xv6-os.html
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "param.h"
 
+#define TIME    300
+#define N       3
 
-int main(void){
-    lottery(5);
+int i = 0;
+
+int main(void) {
+    int pid;
+
+    while(i<N) {
+        pid = fork(NTICKETS * ++i);
+
+        if (pid) {
+            while (1) {
+                printf(1, ">>> pid %d tickets %d\n", pid, NTICKETS*i);
+                sleep(TIME);
+            }
+        }
+
+        if (fork(NTICKETS * ++i)) {
+            while (1) {
+                printf(1, ">>> pid %d tickets %d\n", pid, NTICKETS*i);
+                sleep(TIME);
+            }
+        }
+    }
+
     exit();
 }
--- user.h
+++ user.h
@@ -23,7 +23,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int lottery(int);
+int lottery(void);
 
 // ulib.c
 int stat(const char*, struct stat*);
