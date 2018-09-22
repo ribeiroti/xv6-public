c0c520a0c6120c3aeb01e0245f759302e16c589e
Bruno Ribeiro <brunoribeiroinf@gmail.com>
2018-09-22 18:44:11 -0300

    teste de lottery scheduler com 01 cpu

--- lottery.c
+++ lottery.c
@@ -3,35 +3,30 @@ Created by lucas on 17/09/18.
 
 https://01siddharth.blogspot.com/2018/04/adding-system-call-in-xv6-os.html
 */
-
-
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 #include "param.h"
 
-#define TIME    300
-#define N       5
+#define NFORK 3
+#define N     100
 
-int i = 0;
+int m = 0;
 
 int main(void) {
-    int pid;
+    int pid, i, j, k, l;
 
-    while(i<N) {
-        pid = fork(NTICKETS * ++i);
-
-        if (pid) {
-            while (1) {
-                printf(1, ">>> pid %d tickets %d\n", pid, NTICKETS*i);
-                sleep(TIME);
-            }
-        }
-
-        if (fork(NTICKETS * ++i)) {
-            while (1) {
-                printf(1, ">>> pid %d tickets %d\n", pid, NTICKETS*i);
-                sleep(TIME);
+    while (m < NFORK) {
+        if ((pid=fork(NTICKETS * ++m))) {
+            printf(1, ">>> pid %d tickets %d\n", pid, NTICKETS * m);
+            for (i = 0; i < N; i++) {
+                for (j = 0; j < N; j++) {
+                    for (k = 0; k < N; k++) {
+                        for (l = 0; l < N; l++) {
+                            printf(1, "");
+                        }
+                    }
+                }
             }
         }
     }
